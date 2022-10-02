a586925c6cd4433fdddfe964d7dbee8dc1504fb1
jbt <jbt@SetA-INS>
2022-10-02 10:29:44 +0300

    AAA

--- 2022-10-02-java-eldar/src/a/App3.java
+++ 2022-10-02-java-eldar/src/a/App3.java
@@ -3,15 +3,22 @@ package a;
 public class App3 {
 
 	public static void main(String[] args) {
-		int a = 5;
+		int a = 14;
 		int b = 3;
 		int sum = a + b;
-		
+		int mul = a * b;
+		int div = a / b;
+		int rem = a % b;
+		int sub = a - b;
+
 		System.out.println(a);
 		System.out.println(b);
 		System.out.println(sum);
 		System.out.println("================");
 		System.out.println(a + " + " + b + " = " + sum);
+		System.out.println(a + " * " + b + " = " + mul);
+		System.out.println(a + " / " + b + " = " + div + " remainder: " + rem);
+		System.out.println(a + " - " + b + " = " + sub);
 
 	}
 
---
+++ 2022-10-02-java-eldar/src/a/App4.java
@@ -0,0 +1,21 @@
+package a;
+
+public class App4 {
+
+	public static void main(String[] args) {
+		
+		String firstName = "Eldar";
+		String midleName = "David";
+		String lastName = "Bakshi";
+		
+		System.out.println(firstName);
+		System.out.println(lastName);
+		 
+		System.out.println(firstName + " " + midleName + " " + lastName); 
+		
+		System.out.println("first name\t" + firstName);
+		System.out.println("last name\t" + lastName);
+
+	}
+
+}
