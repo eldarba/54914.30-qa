b5a77800236a496dcf7a467493d63db7798bdf5a
jbt <jbt@603-INS>
2022-10-25 12:20:30 +0300

    cinema

--- 2022-10-25-arrays/src/c/dimensions/Cinema.java
+++ 2022-10-25-arrays/src/c/dimensions/Cinema.java
@@ -1,6 +1,7 @@
 package c.dimensions;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Cinema {
 
@@ -16,6 +17,23 @@ public class Cinema {
 				// System.out.println("row " + row + ", seat " + seat);
 				cinema[row][seat] = 'O';
 			}
+		}
+		
+		cinema[0][0] = 'X';
+		cinema[3][7] = 'X';
+		cinema[2][3] = 'X';
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("enter row: ");
+		int userRow = sc.nextInt() - 1;
+		System.out.print("enter seat: ");
+		int userSeat = sc.nextInt() - 1;
+		sc.close();
+		
+		if(cinema[userRow][userSeat] == 'O') {
+			cinema[userRow][userSeat] = 'X';
+		}else {
+			System.out.println("Sorry, this seat is taken");
 		}
 
 		// print the cinema
