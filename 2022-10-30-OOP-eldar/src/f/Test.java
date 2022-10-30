4cba889c6addf602db16c8ceb295c6c9356326bc
jbt <jbt@603-INS>
2022-10-30 16:11:13 +0200

    OOP

---
+++ 2022-10-30-OOP-eldar/src/f/Test.java
@@ -0,0 +1,17 @@
+package f;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		Person p1 = new Person(101, "Yosi", 25);
+		Person p2 = new Person(102, "Lea", 32);
+		
+		
+		System.out.println(p1);
+		System.out.println(p2);
+		
+
+	}
+
+}
