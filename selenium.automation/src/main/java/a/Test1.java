1648b08460ee3a051465e881dd1777730cbdeeb3
jbt <jbt@603-INS>
2022-11-13 12:28:00 +0200

    selenium

---
+++ selenium.automation/src/main/java/a/Test1.java
@@ -0,0 +1,38 @@
+package a;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test1 {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		// tell java where is the chrome driver
+		String propertyName = "webdriver.chrome.driver";
+		String propertyValue = "C:/eldar/web drivers/chromedriver.exe";
+		System.setProperty(propertyName, propertyValue);
+		
+		// open the chrome browser
+		WebDriver driver = new ChromeDriver();
+		TimeUnit.SECONDS.sleep(1);
+		
+		// maximize the window
+//		driver.manage().window().maximize();
+//		TimeUnit.SECONDS.sleep(1);
+		
+		// navigate to google
+		driver.get("http://www.google.com");
+		TimeUnit.SECONDS.sleep(3);
+		
+		// navigate to ynet
+		driver.get("https://www.ynet.co.il/home/0,7340,L-8,00.html");
+		TimeUnit.SECONDS.sleep(3);
+		
+		// close the window
+		driver.quit();
+
+	}
+
+}
