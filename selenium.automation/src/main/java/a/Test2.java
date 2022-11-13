fb7848f1b77b612095eb489278f6ef880fedf9b5
jbt <jbt@603-INS>
2022-11-13 13:06:47 +0200

    Selenium

--- selenium.automation/src/main/java/a/Test2.java
+++ selenium.automation/src/main/java/a/Test2.java
@@ -10,36 +10,48 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Test2 {
 
 	public static void main(String[] args) throws InterruptedException {
-		
+
 		// tell java where is the chrome driver
 		String propertyName = "webdriver.chrome.driver";
 		String propertyValue = "C:/eldar/web drivers/chromedriver.exe";
 		System.setProperty(propertyName, propertyValue);
-		
+
 		// open the chrome browser
 		WebDriver driver = new ChromeDriver();
-		TimeUnit.SECONDS.sleep(1);
-		
+//		TimeUnit.SECONDS.sleep(1);
+
 		// maximize the window
 //		driver.manage().window().maximize();
 //		TimeUnit.SECONDS.sleep(1);
-		
+
 		// navigate to google
 		driver.get("file://C:/eldar/html/js/demo3.html");
-		TimeUnit.SECONDS.sleep(2);
-		
+//		TimeUnit.SECONDS.sleep(2);
+
 		// find the first h1 element and get its text content
 		WebElement element = driver.findElement(By.tagName("h1"));
 		String text = element.getText();
 		System.out.println(text);
-		TimeUnit.SECONDS.sleep(2);
-		
+//		TimeUnit.SECONDS.sleep(2);
+
 		// get the first input
 		element = driver.findElement(By.id("a"));
 		element.sendKeys("105");
 		// type 105 to the input
+//		TimeUnit.SECONDS.sleep(2);
+
+		// get the second input
+		element = driver.findElement(By.id("b"));
+		element.sendKeys("32");
+		// type 105 to the input
+//		TimeUnit.SECONDS.sleep(2);
+
+		// get the button
+		element = driver.findElement(By.tagName("button"));
+		// click the button
+		element.click();
 		TimeUnit.SECONDS.sleep(5);
-				
+
 		// close the window
 		driver.quit();
 
